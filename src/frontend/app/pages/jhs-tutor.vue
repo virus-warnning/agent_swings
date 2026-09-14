7625b2be56adf7120e8240c8f29815b2dfeb7d07
Raymond Wu <virus.warnning@gmail.com>
2026-09-14 18:30:14 +0800

    Add two items.

---
+++ src/frontend/app/pages/jhs-tutor.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-semibold text-default">國中家教</h1>
+  </div>
+</template>
---
+++ src/frontend/app/pages/vocab-card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-semibold text-default">生字卡</h1>
+  </div>
+</template>
